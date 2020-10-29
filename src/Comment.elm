11187c44928273dea3bc24a3dace6a5668b8731c
Aksel Wester <aksel@wester.co>
2020-10-29 13:47:02 +0100

    Revert "Finish starting point in code"
    This reverts commit b6b72bfbd80c63c786581b265146b7ef03ed3323.

--- src/Comment.elm
+++ src/Comment.elm
@@ -1,6 +1,8 @@
-module Comment exposing (Comment, text, username)
+module Comment exposing (Comment, decoder, subcomments, text, username)
 
 import CommentId exposing (CommentId)
+import Json.Decode exposing (Decoder)
+import Json.Decode.Pipeline exposing (optional, required)
 
 
 type Comment
@@ -11,6 +13,7 @@ type alias CommentInfo =
     { text : String
     , id : CommentId
     , username : String
+    , subcomments : List Comment
     }
 
 
@@ -28,5 +31,25 @@ username (Comment info) =
     info.username
 
 
+subcomments : Comment -> List Comment
+subcomments (Comment info) =
+    info.subcomments
+
+
 
 --- DECODING ---
+
+
+decoder : Decoder Comment
+decoder =
+    commentInfoDecoder
+        |> Json.Decode.map Comment
+
+
+commentInfoDecoder : Decoder CommentInfo
+commentInfoDecoder =
+    Json.Decode.succeed CommentInfo
+        |> required "text" Json.Decode.string
+        |> required "id" CommentId.decode
+        |> required "username" Json.Decode.string
+        |> optional "comments" (Json.Decode.list (Json.Decode.lazy (\_ -> decoder))) []
